7bb18fa9836975a5a7c3843214fd890f91b7c579
SadikSener <sadiksnr1703@gmail.com>
2022-01-16 18:55:23 +0300

    change whereFirstOrNull to whereFirst

--- lib/services/conversations_finder_by_Id.dart
+++ lib/services/conversations_finder_by_Id.dart
@@ -1,12 +1,9 @@
-import 'package:collection/src/iterable_extensions.dart';
-import 'package:twitter/assets/constatns.dart';
 import 'package:twitter/core/init/create_conversations.dart';
 import 'package:twitter/models/conversation_model.dart';
 
-
 //? muhtemelen çok verimsiz bir yol
-Conversation conversationFinderbyId({required String id}){
-  Conversation? conversation = allConversations.firstWhereOrNull((element) => element.conversationUniqueId == id);  
-  if (conversation == null) throw Exception(ErrorMessages.userNotFound);
+Conversation conversationFinderbyId({required String id}) {
+  Conversation? conversation = allConversations
+      .firstWhere((element) => element.conversationUniqueId == id);
   return conversation;
 }
--- lib/services/user_finder_by_email.dart
+++ lib/services/user_finder_by_email.dart
@@ -1,11 +1,8 @@
-import 'package:collection/src/iterable_extensions.dart';
-import 'package:twitter/assets/constatns.dart';
-import 'package:twitter/core/init/create_users.dart';
 import 'package:twitter/models/user_model.dart';
 
 
 //? hangisi daha iyi ?
-UserModel? userFinderByEmail({required String userEmail,required List<UserModel> list}) {
+UserModel? userFinderByEmail({required String userEmail ,required List<UserModel> list}) {
   UserModel? a;
 
   try{
@@ -15,10 +12,4 @@ UserModel? userFinderByEmail({required String userEmail,required List<UserModel>
   }
 
   return a;
-}
-
-UserModel userFinderByEmail2({required String mailOfUser}) {
-  UserModel? user = users.firstWhereOrNull((element) => element.userEmail == mailOfUser);  
-  if (user == null) throw Exception(ErrorMessages.userNotFound);
-  return user;
 }
